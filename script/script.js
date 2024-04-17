39a6a2ad00e1baaf79936043f95c236761376e1a
rositaross <162448303+rositaross@users.noreply.github.com>
2024-04-17 17:39:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-function doLogin() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if(username == "" || password == "") {
-    alert("Username e password sono obbligatori");
+function doLogin() {  
+    var username = document.getElementById("username").value;  
+    var password = document.getElementById("password").value;  
+    if(username == "" || password == "") {    
+      alert("Username e password sono obbligatori");  }
   }
-}
+
