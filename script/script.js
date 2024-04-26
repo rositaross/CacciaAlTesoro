b7ab67734ddfceec8f68311afbf69814caccce53
rositaross <162448303+rositaross@users.noreply.github.com>
2024-04-26 16:54:45 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,20 +6,3 @@ function doLogin() {
     }
   }
 
-else {
-    fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad' , {
-        method: 'POST' ,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-        "username": username,
-        "password": password
-        })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch((error) => {
-        console.error('Errore:', error);
-    });
-}
